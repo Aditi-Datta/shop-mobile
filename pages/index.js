c59ed720dec0b0eabeb81701900781180115f0d2
Aditi-Datta <aditidatta373@gmail.com>
2023-10-08 00:45:39 +0600

    css added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
    <>
-   <h1>Hello from Online Shop in Bangladesh</h1>
+   <div className='bg-teal-950'>
    
+   
+   <Navbar/>
+   <h1 className='text-yellow-500'>Hello from Online Shop in Bangladesh</h1>
+   <button className="btn btn-primary">Hello daisyUI!</button>
+   </div>
    </>
   )
 }
