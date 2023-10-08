9a4cb7cfe249a46508a3bf1ae89a2fa3cad79810
Aditi-Datta <aditidatta373@gmail.com>
2023-10-09 01:02:10 +0600

    adde

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,12 +1,27 @@
+import Image from "next/image"
 import Navbar from "../components/Navbar"
-
+import backgroundImage from "../public/signInbg1.jpg";
 function signIn() {
   return (
     <>
-    <Navbar/>
-    <h1>Please SignIn here</h1>
+      <Navbar />
+      <div style={{
+        backgroundImage: `url(${backgroundImage.src})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+
+
+        <h1 style={{color:'white'}}>Please SignIn here</h1>
+      </div>
     </>
-    
+
   )
 }
 
