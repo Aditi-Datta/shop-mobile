1f7c6749bc3f31f2a98d946b93178c3b46b687ba
Aditi-Datta <aditidatta373@gmail.com>
2023-10-12 02:18:30 +0600

    added

--- pages/about.js
+++ pages/about.js
@@ -4,27 +4,31 @@ import backgroundImage from "../public/about-banner2.png"
 function about() {
   return (
     <>
-    <Navbar/>
-    <div style={{
-      backgroundImage: `url(${backgroundImage.src})`,
-      backgroundPosition: "center",
-      backgroundSize: "cover",
-      
-      width: "100vw",
-      height: "100vh",
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor:'rgba(70,100,19, 0.7)',
-      
-    }}
-    
-    
-    >
-    
-    </div>
-    <h1>Hello from about page</h1>
-    <Footer/>
+      <Navbar />
+      <div style={{
+        backgroundImage: `url(${backgroundImage.src})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+
+        width: "100vw",
+        height: "100vh",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(70,100,19, 0.7)',
+
+      }}
+      >
+
+      <div style={{textAlign:'left', marginLeft:'-620px'}}>
+      <h1 style={{fontSize: '40px', fontWeight: 'bold',}}>Get to Know Us</h1>
+          <h3 >"This is crucial for establishing trust, building <br></br> brand identity, and providing essential information <br></br> about your business to potential customers" </h3>
+          <button className="btn btn-outline btn-wide bg-teal-950 " style={{color:'#c7e32d' ,marginTop:'5%'}}>Learn More</button>
+        </div>
+       
+      </div>
+      <h1>Hello from about page</h1>
+      <Footer />
     </>
   )
 }
--- pages/signIn.js
+++ pages/signIn.js
@@ -19,7 +19,7 @@ function signIn() {
 
 
         <div className="
-        max-w-2xl mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl"
+        rounded-xl shadow-md overflow-hidden  max-w-2xl mx-auto md:max-w-2xl "
           style={{
             backgroundImage: `url(${backgroundImage.src})`,
             backgroundRepeat: "no-repeat",
@@ -30,8 +30,13 @@ function signIn() {
           }}
         >
           <div class="md:flex">
+            
             <div class="md:shrink-0">
-              <h1 style={{ color: 'white', fontSize: '70px', fontWeight: 'bold', }} className="p-8">Welcome!</h1>
+              <h1 style={{ color: 'white', fontSize: '70px', fontWeight: 'bold', }} className="px-8 pt-8">Welcome!</h1>
+              <p style={{color: 'white', paddingLeft: '12%', 
+              
+              
+              }}>Discover a world of limitless possibilities <br></br> in our online store. 🌟 We're thrilled to have <br></br> you here, and we can't wait to help you <br></br> find exactly what you're looking for.</p>
             </div>
 
             <div className=" mx-5 py-12">
@@ -53,7 +58,7 @@ function signIn() {
              focus:border-sky-500 focus:ring-sky-500 block
              w-full rounded-md sm:text-sm focus:ring-1" placeholder="your name" />
                 </label>
-                <button className="btn btn-sm bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 px-24 mt-4">Submit</button>
+                <button className="btn btn-block bg-teal-950 hover:bg-teal-700 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 px-24 mt-4" style={{color:'#c7e32d'}}>Submit</button>
 
               </form>
             </div>
